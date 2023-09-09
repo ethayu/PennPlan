24e64eae854142f55aea2b05618ad898c3d19609
Samuel Ngiam <samuelngiam@lev-13426.apn.wlan.private.upenn.edu>
2023-09-09 02:12:51 -0400

    changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,33 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen flex flex-col">
       <nav class="bg-darkBlue border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" class="flex items-center">
               <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
               <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PennPlan</span>
           </a>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          </div>
         </div>
       </nav>
 
-      <div class="flex">
-        <div class="flex flex-col align-center justify-center basis-1/4 bg-white h-screen">
-            {/* <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Default</label> */}
+      <div class="flex flex-row h-full">
+        <div class="flex flex-col align-center justify-center basis-1/4 bg-white">
             <input type="text" id="default-input" class="my-3 self-center bg-gray-50 border border-lightGray text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-45 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Major'/>
-            {/* <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Default 2</label> */}
             <input type="text" id="default-input" class="my-3 self-center bg-gray-50 border border-lightGray text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-45 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Minor'/>
             <input type="text" id="default-input" class="my-3 self-center bg-gray-50 border border-lightGray text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-45 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Semesters left'/>
             <input type="text" id="default-input" class="my-3 self-center bg-gray-50 border border-lightGray text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-45 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Interests'/>
             <input type="text" id="default-input" class="my-3 self-center bg-gray-50 border border-lightGray text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-45 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Classes taken'/>
             <button type="submit" class="my-3 self-center text-white bg-medBlue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-32 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </div>
-        <div class="flex-auto basis-3/4 bg-lightBlue h-screen">
+        <div class="flex flex-col basis-3/4 bg-lightBlue">
           02
-          <div class="grid grid-rows-2 grid-flow-col bg-red justify-center gap-x-8">
-            <div class="w-60 bg-orange h-3/6">
+          <div class="grow grid grid-rows-2 grid-flow-col auto-cols-max place-content-around content-center gap-8 bg-red justify-center gap-x-8">
+            <div class="w-60 bg-orange">
             a
             </div>
             <div class="w-60 bg-orange">
