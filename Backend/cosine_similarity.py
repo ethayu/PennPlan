6aa18b4b47528c4cf49f6d1f6d709fa413a13598
kuomat <kuomatthew20040529@gmail.com>
2023-09-10 07:27:38 -0400

    Cosine similarity

---
+++ Backend/cosine_similarity.py
@@ -0,0 +1,54 @@
+from transformers import BertTokenizer, BertModel
+import torch
+import numpy as np
+
+from populate import *
+
+
+## Convert 'Natural Science Electives' to embeddings
+keyword = 'Natural Science Electives'
+
+# Create the model
+model_name = 'bert-base-uncased'
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertModel.from_pretrained(model_name)
+
+tokens = tokenizer(keyword, return_tensors='pt')
+with torch.no_grad():
+    outputs = model(**tokens)
+    keyword_embeddings = outputs.last_hidden_state[0][0]
+    keyword_embeddings = keyword_embeddings.reshape(768, 1)
+
+# Cosine similarity method
+from sklearn.metrics.pairwise import cosine_similarity
+
+def find_most_similar_vectors(query_vector, vectors_collection, num_similar_vectors=3):
+    similarities = cosine_similarity(query_vector.T, vectors_collection)
+    most_similar_indices = np.argsort(similarities[0])[::-1]
+    top_similar_vectors = vectors_collection[most_similar_indices[:num_similar_vectors]]
+    return top_similar_vectors
+
+
+
+
+# # Query to get all the embeddings in the database
+db = setup()
+cursor = db.Courses.find()
+all_entries = [entry for entry in cursor]
+all_names = [entry['title'] for entry in all_entries]
+all_embeddings = [entry['embedding'] for entry in all_entries]
+all_embeddings_np = np.array(all_embeddings)
+
+
+# all_embeddings_tuples = [tuple(embedding) for embedding in all_embeddings]
+# embeddings_to_name = dict(zip(all_embeddings_tuples, all_names))
+
+print(keyword_embeddings.shape)
+print(all_embeddings_np.shape)
+
+top_k_similar_embeddings = find_most_similar_vectors(keyword_embeddings, all_embeddings_np, num_similar_vectors=2)
+print(top_k_similar_embeddings.shape)
+top_k_similar_embeddings_tuple = [tuple(embedding) for embedding in top_k_similar_embeddings]
+
+top_k_similar_classes = [embeddings_to_name[embedding] for embedding in top_k_similar_embeddings_tuple]
+print(top_k_similar_classes)
---
+++ Backend/populate.py
@@ -0,0 +1,102 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import json
+
+
+def setup():
+    uri = "mongodb+srv://ethayu1:dMyZtC65r7tEA8Ge@cluster0.sb78cwt.mongodb.net/?retryWrites=true&w=majority"
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    return client.Database
+
+class PCR_Data:
+    def __init__(self, course_quality, difficulty, work_required):
+        self.course_quality = course_quality
+        self.difficulty = difficulty
+        self.work_required = work_required
+
+#create course class
+class Course:
+    def __init__(self, title, name, description, prereqs, pcr_data: PCR_Data, embedding):
+        self.title = title
+        self.name = name
+        self.description = description
+        self.prereqs = prereqs
+        self.pcr_data = pcr_data
+        self.embedding = embedding
+    def jsontoCourse(json):
+        return Course(json["title"], json["name"], json["description"], json["prereqs"], PCR_Data(json["pcr_data"]["course_quality"], json["pcr_data"]["difficulty"], json["pcr_data"]["work_required"]), json["embedding"])
+
+
+class Field:
+    def __init__(self, name, reqs, electives): #to complete field, need to take courses in reqs, and $electives.value courses from $electives.key
+        self.name = name
+        self.reqs = reqs
+        self.electives = electives
+
+# class Minor:
+#     def __init__(self, name, reqs, electives, course_bank, number): #to complete minor, need to take courses in reqs, and $number courses from $course_bank
+#         self.name = name
+#         self.reqs = reqs
+#         self.electives = electives
+#         self.course_bank = course_bank
+#         self.number = number
+
+class Concentration:
+    def __init__(self, name, reqs, electives, course_bank, number): #to complete concentration, need to take courses in reqs, and $number courses from $course_bank
+        self.name = name
+        self.reqs = reqs
+        self.electives = electives
+        self.course_bank = course_bank
+        self.number = number
+
+def addCourse(course: Course, db):
+    courses = db.Courses
+    json = vars(course)
+    json['pcr_data'] = vars(course.pcr_data)
+    courses.insert_one(json)
+
+def addMajor(major: Field, db):
+    majors = db.Majors
+    majors.insert_one(vars(major))
+
+# def addMinor(minor: Minor, db):
+#     minors = db.Minors
+#     minors.insert_one(vars(minor))
+
+
+# Load number: course title
+# with open("course_name_map.json", 'r') as f:
+#     number_title = json.load(f)
+
+# # Load number: course description
+# with open("course_description_map.json", 'r') as f:
+#     number_description = json.load(f)
+
+# # Load number: pcr
+# with open("course_review.json", 'r') as f:
+#     number_pcr = json.load(f)
+
+# # Load number: prerequisites
+# with open("prerequisites.json", 'r') as f:
+#     number_prereqs = json.load(f)
+
+
+
+# print(number_description)
+# db = setup()
+# CIS160 = Course(title='CIS 1600',
+#                 name='Mathematical Foundations of Computer Science',
+#                 description='This course provides an introduction to the mathematical foundations of computer science. Topics include logic, set theory, induction, functions and relations, combinatorics and graph theory.',
+#                 prereqs=['CIS 1200', 'CIS 1600'],
+#                 pcr_data=PCR_Data(4.5, 3.5, 5.0),
+#                 embedding=[1, 2, 3, 4, 5])
+# addCourse(CIS160, db)
